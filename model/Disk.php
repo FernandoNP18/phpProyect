abaecc9a7ea71b211d903f46a5087358bf75da01
Fernando <fernandojosemateos.18@campuscamara.es>
2020-01-30 16:23:38 +0100

    db

--- model/Disk.php
+++ model/Disk.php
@@ -1,5 +1,62 @@
 <?php
+require_once("../controller/connectorDB.php");
 class Disk{
+    private $image;
+    private $name;
+    private $genre;
+    private $author;
+    private $prize;
+    private $songs;
+    private $stock;
+    private $db;
+    public function __construct($name,$image,$genre,$author,$prize,$songs,$stock)
+    {
+        //Verify if the data is correct in the db
+        if(is_float($prize) && is_int($stock) && $this->checkName(30,$name)&&$this->checkName(30,$genre)&&$this->checkName(30,$author)&&$this->checkName(100,$songs)&&
+                $prize>0 && $prize<=50.00 && $stock>0){
+                $this->name="'$name'";
+                $this->genre="'$genre'";
+                $this->author="'$author'";
+                $this->prize=floatval($prize);
+                $this->songs="'$songs'";
+                $this->stock=intval($stock);
+                $this->image="'../CSS/IMG/$image'";
+                $this->db=contactDB("localhost","USER","root","");
+            }else{
+                echo "<script>alert('Error al introducir los datos')</script>";
+            }
+    }
 
+    private function checkName($len,$name){
+        return !empty($name) && strlen($name)<=$len && preg_match("/[a-zA-Z ]+/",$name);
+    }
+    public function searchFor($name,$author,$genre,$prize){
+        $sql="";
+        $where="";
+        if(!empty($name)){
+            $sql.="NAME ";
+            $where.="NAME LIKE '%$name%' ";
+        }
+        if(!empty($author)){
+            $sql.="AUTHOR ";
+            $where.="AUTHOR LIKE '%$author%' ";
+        }
+        if(!empty($genre)){
+            $sql.="GENRE ";
+            $where.="GENRE LIKE '%$genre%' ";
+        }
+        if(!empty($genre)){
+            $sql.="PRIZE ";
+            $where.="PRIZE=$prize' ";
+        }
+        $sql=str_replace(" ",",",trim($sql));
+        $where=str_replace(" ","AND",trim($where));
+        if($sql=""){
+            header("location: searchDisk.php");
+        }else{
+            $select=$this->db->prepare("SELECT $sql FROM DISK WHERE $where");
+            $select->execute();
+        }
+    }
 }
 ?>
--- model/User.php
+++ model/User.php
@@ -1,4 +1,5 @@
 <?php
+@ require_once("../controller/connectorDB.php");
 class User{
     private $dni;
     private $name;
@@ -8,31 +9,50 @@ class User{
     private $email;
     private $db;
    function __construct($dni,$name,$username,$surname,$password,$email){
-    
+    //Check if the new user is correct
+    if($this->checkDni($dni) && $this->checkEmail(50,$email) && $this->checkUser(30,$username)&& $this->checkUser(40,$password) && $this->checkName(50,$surname) && $this->checkName(30,$name)){
+        $this->dni=$dni;
+        $this->name=$name;
+        $this->username=$username;
+        $this->surname=$surname;
+        $this->email=$email;
+        $this->password=$password;
+        //Conect to the DB
+        $this->db=contactDB("localhost","USER","root","");
+    }else{
+        echo "<script>alert('Error al introducir los datos')</script>";
+    }
    }
-   public function getUser($dni){
-
-   }
+   //Insert into db the new user
    public function insert(){
-    $insert=$this->db->prepare('INSERT INTO USERS VALUES(:dni,:name,:surname,:username,:password,:email)');
+    $insert=$this->db->prepare("INSERT INTO USERS VALUES('$this->dni','$this->name','$this->surname','$this->username','$this->password','$this->email')");
    }
+   //Check dni
    private function checkDni($dni){
-       return !empty($dni) && strlen($dni)==9 && preg_match("/\d{8}[A-Z-a-z]/",$dni) && strtoupper(substr($dni,-1,1))=="TRWAGMYFPDXBNJZSQVHLCKE"[intval(substr($dni,0))%23];
+       return !empty($dni) && strlen($dni)==9 &&
+        preg_match("/\d{8}[A-Z-a-z]/",$dni) &&
+        strtoupper(substr($dni,-1,1))=="TRWAGMYFPDXBNJZSQVHLCKE"[intval(substr($dni,0))%23] &&
+        $this->dniExist($dni);
    }
+   //check names and usernames
    private function checkName($len,$name){
        return !empty($name) && strlen($name)<=$len && preg_match("/[a-zA-Z ]+/",$name);
    }
+   //verify email
    private function checkEmail($len,$email){
     return !empty($email) && strlen($email)<=$len && filter_var($email, FILTER_VALIDATE_EMAIL);
    }
+   //check usernames or passwords
    private function checkUser($len,$us){
        return !empty($us) && strlen($us)<=$len && preg_match("/[a-zA-Z0-9%_$+/",$us);
-   }
-   public static function checkUserExists($name,$password){
-   
-   }
-   private function dniExist($dni){
-   
     }
+   //Check if the dni already exist on the db
+   private function dniExist($dni){
+    return $this->db->query("SELECT DNI FROM USERS WHERE DNI='$dni'")==0; 
+   }
+    //Check if the user exists
+   public static function checkUserExists($name,$password){
+    return $this->db->query("SELECT NAME, PASSWORD FROM USERS WHERE '$name'=NAME AND '$password'=PASSWORD")==1;
+   }
 }
 ?>
